8081be1a9927bcfb871faab38f65f390bedc5655
Donzaffi <47207943+Donzaffi@users.noreply.github.com>
2026-06-28 16:15:09 +0200

    Update run.sh

--- domusa_bridge/run.sh
+++ domusa_bridge/run.sh
@@ -4,5 +4,6 @@ bashio::log.info "Starting Domusa MQTT Bridge..."
 
 export USERNAME=$(bashio::config 'username')
 export PASSWORD=$(bashio::config 'password')
+export POLL_INTERVAL=$(bashio::config 'poll_interval')
 
 python3 /app/main.py
