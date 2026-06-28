4261a1448f14e37fb55ddb40236e55373e22226b
Donzaffi <47207943+Donzaffi@users.noreply.github.com>
2026-06-28 16:37:17 +0200

    Update router.py

--- domusa_bridge/src/router.py
+++ domusa_bridge/src/router.py
@@ -2,21 +2,24 @@ import asyncio
 
 
 class Router:
-    def __init__(self, api, mqtt, device, cfg):
+    def __init__(self, api, mqtt, device):
         self.api = api
         self.mqtt = mqtt
         self.device = device
-        self.cfg = cfg
 
     async def listen(self):
         cid = self.device["id"]
 
-        async with self.mqtt.client.filtered_messages(f"domusa/{cid}/set/#") as msgs:
-            await self.mqtt.client.subscribe(f"domusa/{cid}/set/#")
+        await self.mqtt.client.subscribe(f"domusa/{cid}/set/#")
 
-            async for msg in msgs:
-                key = msg.topic.value.split("/")[-1]
-                value = msg.payload.decode()
+        async with self.mqtt.client.messages() as messages:
+            async for msg in messages:
+                try:
+                    key = msg.topic.value.split("/")[-1]
+                    value = msg.payload.decode()
 
-                if key == "tempConsigna":
-                    await self.api.set_temp(cid, float(value), self.cfg.zone)
+                    if key == "tempConsigna":
+                        await self.api.set_temp(cid, float(value))
+
+                except Exception as e:
+                    print("Router error:", e)
