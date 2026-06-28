fafd907534e0c554c80b1f6e2e6a28b631b873ad
Donzaffi <47207943+Donzaffi@users.noreply.github.com>
2026-06-28 16:17:42 +0200

    Create mqtt_client.py

---
+++ domusa_bridge/src/mqtt_client.py
@@ -0,0 +1,12 @@
+from aiomqtt import Client
+
+
+class MQTTClient:
+    def __init__(self):
+        self.client = Client("core-mosquitto")
+
+    async def connect(self):
+        await self.client.__aenter__()
+
+    async def publish(self, topic, payload):
+        await self.client.publish(topic, payload)
