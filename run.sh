1053bb571243f68da6c23ecfb504d38e578e5579
Donzaffi <47207943+Donzaffi@users.noreply.github.com>
2026-06-28 13:07:02 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/with-contenv bashio
 
 # Optional: Logging in HA anzeigen
-bashio::log.info "Starting Caldera MQTT Bridge..."
+bashio::log.info "Starting Domusa MQTT Bridge..."
 
 # Starte das Python-Skript
 python3 /app/main.py
