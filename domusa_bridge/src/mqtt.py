71a563871656907337a4f351248b9f6b2992331c
Donzaffi <47207943+Donzaffi@users.noreply.github.com>
2026-06-28 18:17:02 +0200

    Update mqtt.py

--- domusa_bridge/src/mqtt.py
+++ domusa_bridge/src/mqtt.py
@@ -1,12 +1,20 @@
-from aiomqtt import Client
-
+import aiomqtt
 
 class MQTT:
-    def __init__(self):
-        self.client = Client("core-mosquitto")
+    def __init__(self, host, port, user, password):
+        self.host = host
+        self.port = port
+        self.user = user
+        self.password = password
+        self.client = None
 
     async def connect(self):
+        print(f"DEBUG: Verbinde mit MQTT {self.host} als User {self.user}...")
+        # Hier müssen user und password zwingend übergeben werden
+        self.client = aiomqtt.Client(
+            hostname=self.host,
+            port=self.port,
+            username=self.user,
+            password=self.password
+        )
         await self.client.__aenter__()
-
-    async def publish(self, topic, payload):
-        await self.client.publish(topic, payload)
