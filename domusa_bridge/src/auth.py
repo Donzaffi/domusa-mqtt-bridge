045e1c4795ca23c33ff9b61f353fd77d008a0481
Donzaffi <47207943+Donzaffi@users.noreply.github.com>
2026-06-28 16:36:04 +0200

    Update auth.py

--- domusa_bridge/src/auth.py
+++ domusa_bridge/src/auth.py
@@ -1,36 +1,18 @@
 import aiohttp
-import time
-import json
-import os
 
 
 class Auth:
-    def __init__(self, cfg):
-        self.cfg = cfg
-        self.base_url = "https://ic-api-app.azurewebsites.net"
-        self.token_file = "/data/token.json"
+    BASE = "https://ic-api-app.azurewebsites.net"
 
-    async def login(self):
+    async def get_token(self):
         async with aiohttp.ClientSession() as s:
             r = await s.post(
-                f"{self.base_url}/v1/auth/login",
+                f"{self.BASE}/v1/auth/login",
                 json={
-                    "username": self.cfg.username,
-                    "password": self.cfg.password,
+                    "username": "",
+                    "password": "",
                     "langDevice": "es"
                 }
             )
-            return await r.json()
-
-    async def get_valid_token(self):
-        data = await self.login()
-
-        token = {
-            "access": data["token"],
-            "expires": time.time() + 3600
-        }
-
-        with open(self.token_file, "w") as f:
-            json.dump(token, f)
-
-        return token["access"]
+            data = await r.json()
+            return data["token"]
