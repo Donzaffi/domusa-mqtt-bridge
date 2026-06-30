1417d6e16e42891b596210f1af0b19c6f720a0c2
Donzaffi <47207943+Donzaffi@users.noreply.github.com>
2026-06-30 14:28:04 +0200

    Update main.py

--- domusa_bridge/src/main.py
+++ domusa_bridge/src/main.py
@@ -44,6 +44,8 @@ async def main():
     
     # Sprachwahl aus Config laden (Standard: 'de')
     lang = config.get("language", "de")
+    if lang not in ["de", "en"]:
+    lang = "de" # Fallback, falls jemand etwas Falsches eintippt
     
     # Authentifizierung und API
     auth = Auth(config["username"], config["password"])
